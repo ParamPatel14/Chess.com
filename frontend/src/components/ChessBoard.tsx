2353b8fb1d748029bafb144c7d49f0f22db7a547
ParamPatel14 <parampraman59@gmail.com>
2025-03-30 10:30:20 +0530

    frontend1

---
+++ frontend/src/components/ChessBoard.tsx
@@ -0,0 +1,7 @@
+
+export const ChessBoard = () => {
+    return <div className="text-white-200">
+        ChessBoard
+
+    </div>
+}
--- frontend/src/screens/Landing.tsx
+++ frontend/src/screens/Landing.tsx
@@ -1,17 +1,26 @@
 
+import { useNavigate } from "react-router-dom";
+import { Button } from "../components/Button";
+
 export const Landing = () => {
-    return <div>
-        <div className="mt-2">
+    const navigate = useNavigate();
+
+    return <div className="flex justify-center">
+        <div className="pt-8 max-w-screen-lg">
             <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
-                <div>
-                    <img src={"/chessboard.jpeg"} />
+                <div className="flex-justify-center">
+                    <img src={"/chessboard.jpeg"} className="max-w-96"/>
                 </div>
-                <div>
-                    <h1 className="text-3xl font-bold">Play chess online on the #2 Site!</h1>
-                    <div className="mt-4">
-                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                            Play Online
-                        </button>
+                <div className="pt-16">
+                    <div className="flex justify-center">
+                        <h1 className="text-4xl font-bold text-white">Play Chess online on the #2 Site!</h1>
+                    </div>
+                    <div className="mt-8 flex justify-center">
+                        <Button onClick={() => {
+                            navigate("/game")
+                        }}>
+                            Play Online!
+                        </Button>
                     </div>
                 </div>
             </div>
